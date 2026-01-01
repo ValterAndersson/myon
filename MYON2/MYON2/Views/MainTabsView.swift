63360a303bd71f7533c81c446e7b0703e2955202
halycon575 <mrhalycon@gmail.com>
2026-01-01 13:03:05 +0200

    chore(ios): remove unused Templates, Routines, and Dev views
    Deleted files:
    - MYON2/MYON2/UI/Templates/ (TemplatesListView, TemplateDetailView)
    - MYON2/MYON2/UI/Routines/ (RoutinesListView, RoutineDetailView, RoutineEditView)
    - MYON2/MYON2/Views/ComponentGallery.swift
    - MYON2/MYON2/ViewModels/RoutinesViewModel.swift
    - MYON2/MYON2/Models/Routine.swift
    - MYON2/MYON2/Repositories/RoutineRepository.swift
    - MYON2/MYON2/Repositories/TemplateRepository.swift
    
    MainTabsView now only has Home + Start Workout tabs.

--- MYON2/MYON2/Views/MainTabsView.swift
+++ MYON2/MYON2/Views/MainTabsView.swift
@@ -2,9 +2,6 @@ import SwiftUI
 
 enum MainTab {
     case home, workout
-    #if DEBUG
-    case components
-    #endif
 }
 
 struct MainTabsView: View {
@@ -25,15 +22,6 @@ struct MainTabsView: View {
             }
             .tabItem { Label("Start Workout", systemImage: "figure.strengthtraining.traditional") }
             .tag(MainTab.workout)
-
-            // Components Gallery (Development only)
-            #if DEBUG
-            NavigationStack {
-                ComponentGallery()
-            }
-            .tabItem { Label("Dev", systemImage: "rectangle.3.offgrid") }
-            .tag(MainTab.components)
-            #endif
         }
     }
 }
